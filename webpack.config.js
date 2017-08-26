b1e1764a2b29ab86378c0078d9fd8d287f024911
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-26 17:36:35 -0400

    added a webpack config file, and a main.js file to build out the HTML template

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: './dist',
+        filename: 'build.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
