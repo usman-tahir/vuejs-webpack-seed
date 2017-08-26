b48ae1b482b92b4094db6aaa7f22b593f86a9e9a
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-08-26 18:54:44 -0400

    refactored the Vue app definition with an app loader

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="message"></div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: 'Hello, vue-loader'
+            }
+        }
+    }    
+</script>
+
+<style>
+    .message {
+        color: blue;
+    }
+</style>
